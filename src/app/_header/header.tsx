e1d570c18480421419d7ed54e667b37d26fa9730
rsastri21 <rsastri21@gmail.com>
2024-06-29 16:22:56 -0400

    Basic setup with theming and navbar.

---
+++ src/app/_header/header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { ModeToggle } from "./theme-toggle";
+import { Button } from "@/components/ui/button";
+
+export function Header() {
+    return (
+        <nav className="border-b py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex gap-8 items-center">
+                    <Button variant="ghost">
+                        <Link
+                            href="/"
+                            className="flex gap-2 items-center text-2xl font-semibold"
+                        >
+                            Crew Match
+                        </Link>
+                    </Button>
+                </div>
+                <div className="flex items-center justify-between gap-5">
+                    <HeaderActions />
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+function HeaderActions() {
+    return (
+        <>
+            <ModeToggle />
+        </>
+    );
+}
