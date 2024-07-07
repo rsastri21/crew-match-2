8ef26d7a07a6aff859e1e59e30935b5e319718bd
rsastri21 <rsastri21@gmail.com>
2024-07-06 21:49:01 -0400

    Added support for local docker database.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,22 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import {
+    drizzle as NeonDrizzle,
+    NeonHttpDatabase,
+} from "drizzle-orm/neon-http";
+import {
+    drizzle as LocalDrizzle,
+    PostgresJsDatabase,
+} from "drizzle-orm/postgres-js";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
+import postgres from "postgres";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+let db: PostgresJsDatabase<typeof schema> | NeonHttpDatabase<typeof schema>;
+if (process.env.NODE_ENV === "production") {
+    const sql = neon(process.env.DATABASE_URL!);
+    db = NeonDrizzle(sql, { schema });
+} else {
+    const queryClient = postgres(process.env.DATABASE_URL!);
+    db = LocalDrizzle(queryClient, { schema });
+}
+
+export { db };
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,15 +1,38 @@
 import "dotenv/config";
 
-import { migrate } from "drizzle-orm/neon-http/migrator";
+import { migrate as NeonMigrate } from "drizzle-orm/neon-http/migrator";
+import { migrate as PostgresMigrate } from "drizzle-orm/postgres-js/migrator";
 import { db } from "./index";
+import * as schema from "./schema";
+import { NeonHttpDatabase } from "drizzle-orm/neon-http";
+import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 const main = async () => {
-    try {
-        await migrate(db, { migrationsFolder: "./src/db/migrations" });
-        console.log("Migration completed.");
-    } catch (error) {
-        console.error("Error during migration:", error);
-        process.exit(1);
+    if (process.env.NODE_ENV === "production") {
+        try {
+            await NeonMigrate(db as NeonHttpDatabase<typeof schema>, {
+                migrationsFolder: "./src/db/migrations",
+            });
+            console.log("Migration completed.");
+        } catch (error) {
+            console.error("Error during migration:", error);
+            process.exit(1);
+        }
+    } else {
+        try {
+            const migrationClient = postgres(process.env.DATABASE_URL!, {
+                max: 1,
+            });
+            await PostgresMigrate(drizzle(migrationClient), {
+                migrationsFolder: "./src/db/migrations",
+            });
+            await migrationClient.end();
+            console.log("Migration completed.");
+        } catch (error) {
+            console.error("Error during migration:", error);
+            process.exit(1);
+        }
     }
 };
 
