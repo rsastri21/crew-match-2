28fda7488b8aa72451f9532aa5c2ec1444f5560a
rsastri21 <rsastri21@gmail.com>
2024-10-06 20:13:26 -0400

    Button to create production when user hasn't created one.

--- src/app/production-head/dashboard/_components/MyProductionSection.tsx
+++ src/app/production-head/dashboard/_components/MyProductionSection.tsx
@@ -2,8 +2,11 @@
 
 import ProductionInfoCard from "@/components/ProductionInfoCard";
 import SectionHeading from "@/components/SectionHeading";
+import { buttonVariants } from "@/components/ui/button";
 import { ProductionWithRoles } from "@/db/schema";
+import { cn } from "@/lib/utils";
 import { getAmountFilled } from "@/utils/productionClientUtils";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 interface MyProductionSectionProps {
@@ -20,8 +23,19 @@ export default function MyProductionSection(props: MyProductionSectionProps) {
 
     return (
         <div className="w-full flex flex-col gap-2 pb-6 md:justify-center items-center">
-            <div className="w-full flex justify-start">
+            <div className="w-full flex justify-between items-center">
                 <SectionHeading title="My Production" />
+                {!props.production && (
+                    <Link
+                        href="/production/create"
+                        className={cn(
+                            buttonVariants({ variant: "default" }),
+                            "w-fit"
+                        )}
+                    >
+                        Create Production
+                    </Link>
+                )}
             </div>
             {props.production ? (
                 <div className="w-full px-2">
